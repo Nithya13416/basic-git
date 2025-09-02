6031a8ee99e267b3a66abc7b00c35e3fb9ae88a7
nithyashree <nithyashree.vijaya@kyndryl.com>
2025-09-02 18:28:56 +0530

    added new file

---
+++ add.py
@@ -0,0 +1,11 @@
+# add_two_numbers.py
+
+# take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# add the numbers
+sum_result = num1 + num2
+
+# display the result
+print("The sum is:", sum_result)
